fa456a4cd341bffcd0b3ea434b85566589be6862
DaniOrtunyo <daniortunomurcia@gmail.com>
2025-03-17 14:04:25 +0100

    Añado cambios a una nueva rama

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Dani!");
+        System.out.println("Hola Dani! Esto es una nueva rama");
     }
 }
